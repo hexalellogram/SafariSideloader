ea84f7f0537ca4865838d1c141945fbdde5d9e30
Bryce Tsuyuki <btsuyuki1@gmail.com>
2020-06-30 14:34:16 -0700

    add basic framework for conversion process, and basic URL detection

---
+++ backend/convert.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+
+def downloadChromeExtension(url):
+    print("Downloading Chrome Extension")
+
+def downloadFirefoxExtension(url):
+    print("Downloading Firefox Extension")
+
+def downloadExtension(url):
+    print("Downloading Extension")
+    if "chrome.google.com/webstore/detail" in url:
+        print ("Chrome Web Store URL detected")
+    elif "addons.mozilla.org/en-US/firefox/addon" in url:
+        print ("Firefox Extension URL detected")
+    else:
+        print("Invalid URL detected! Please pass in the URL of the extension's page in the Chrome Web Store or Firefox Browser Add-Ons gallery.")
+
+def extractExtension():
+    print("extract")
+
+def convertExtension():
+    print("convert")
+
+def compileXcodeProject():
+    print("compile")
+
+def runXcodeProject():
+    print("run")
+
+downloadExtension(sys.argv[1])
+extractExtension()
+compileXcodeProject()
+runXcodeProject()
